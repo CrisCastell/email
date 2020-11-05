f284bb8fee1ebf467caa3b876727c259aa2cfa7c
CrisCastell <cristiamcas2000@gmail.com>
2020-11-04 22:07:00 -0400

    Updated allowed hosts

--- mail/settings.py
+++ mail/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('EMAIL_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://cristiamail.herokuapp.com']
+ALLOWED_HOSTS = ['cristiamail.herokuapp.com']
 
 
 # Application definition
